780b9c94948a1fd7291fb6c9b9fcbf58554ab25b
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2013-04-25 16:13:04 -0700

    fixing ruby mode

--- emacs/init.el
+++ emacs/init.el
@@ -10,11 +10,17 @@
 ;; Use cperl-mode instead of the default perl-mode
 (defalias 'perl-mode 'cperl-mode)
 
-;; Use 4 space indents via cperl mode
+;; Use 4 space indents for cperl mode
 (custom-set-variables
   '(cperl-close-paren-offset -4)
   '(cperl-continued-statement-offset 4)
   '(cperl-indent-level 4)
   '(cperl-indent-parens-as-block t)
   '(cperl-tab-always-indent t)
+)
+
+;; Use 4 space indents for ruby mode
+(custom-set-variables
+  '(ruby-deep-indent-paren nil)
+  '(ruby-indent-level 4)
 )
