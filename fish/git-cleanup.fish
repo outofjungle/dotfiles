0e03f74ea06f8f5ddefe0bde88786e243915b269
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2017-03-09 15:20:14 -0800

    fixing git branch delete in git-cleanup

--- fish/git-cleanup.fish
+++ fish/git-cleanup.fish
@@ -3,5 +3,13 @@ function git-cleanup
     git fetch
     git checkout master
     git rebase origin/master
-    git stash pop
+
+    if test $status -eq 0
+        git stash pop
+        for x in (git branch | string trim --)
+            if [ $x != "* master" ]
+                git branch -D $x
+            end
+        end
+    end
 end
