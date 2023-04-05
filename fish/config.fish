35f45a5c38c44219eb24a374dfbae0dbe1ee1767
Venkat Venkataraju <vvenkataraju@splunk.com>
2023-04-05 10:27:23 -0700

    Adding fish config

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+eval (/opt/homebrew/bin/brew shellenv)
+starship init fish | source
+set -gx SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
