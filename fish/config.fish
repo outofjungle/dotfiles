772135ef3b182197233f033aabaab45296d23ecc
Venkat Venkataraju <venkat.venkataraju@gmail.com>
2026-01-26 20:38:11 -0800

    init function for cynthion

--- fish/config.fish
+++ fish/config.fish
@@ -13,17 +13,8 @@ fish_add_path -g $HOME/go/bin
 fish_add_path -g /opt/homebrew/opt/rustup/bin
 fish_add_path -g $HOME/.cargo/bin
 
-# Function to activate ESP-IDF/Matter environment (run once per terminal)
-function idf-init
-    # Local development paths - customize these for your setup
-    set -gx IDF_PATH /Users/ven/Workspace/ESP/esp-idf
-    set -gx ESP_MATTER_PATH /Users/ven/Workspace/ESP/esp-matter
-    set -gx _PW_ACTUAL_ENVIRONMENT_ROOT $ESP_MATTER_PATH/connectedhomeip/connectedhomeip/.environment
+# Activate ESP-IDF/Matter environment (run once per terminal)
+source ~/Workspace/dotfiles/fish/init-idf.fish
 
-    # Pigweed tools for Matter builds (must be in PATH before export.fish)
-    set -gx PATH $_PW_ACTUAL_ENVIRONMENT_ROOT/cipd/packages/pigweed $_PW_ACTUAL_ENVIRONMENT_ROOT/cipd/packages/pigweed/bin $PATH
-
-    # ESP-IDF export.fish expects 'python' command
-    alias python=python3
-    source $IDF_PATH/export.fish
-end
+# Load Cynthion development environment function
+source ~/Workspace/dotfiles/fish/init-cynthion.fish
